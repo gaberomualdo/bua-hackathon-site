9b8ae76849094106ff10db57ab6c097de3387d7b
xtrp <36894883+xtrp@users.noreply.github.com>
2022-10-26 15:20:48 -0400

    feat: new logo

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ export default function App() {
         <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="/">
-              <img className="h-10 w-auto sm:h-12" src="/logo.jpg" alt="" />
+              <img className="h-12 w-auto sm:h-14" src="/logo.jpg" alt="" />
             </a>
           </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
--- src/Banner.js
+++ src/Banner.js
@@ -1,16 +1,17 @@
 export function Banner(props) {
-  return (
-    <div className="bg-gray-800">
-      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-center flex-wrap">
-          <p className="ml-3 font-medium text-white truncate">
-            <span className="md:hidden">We announced a new product!</span>
-            <span className="hidden text-lg md:inline">
-              Registration ends on March 20, 2022. Sign up now!
-            </span>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  return null;
+  // return (
+  //   <div className="bg-gray-800">
+  //     <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+  //       <div className="flex items-center justify-center flex-wrap">
+  //         <p className="ml-3 font-medium text-white truncate">
+  //           <span className="md:hidden">We announced a new product!</span>
+  //           <span className="hidden text-lg md:inline">
+  //             Registration ends on March 20, 2022. Sign up now!
+  //           </span>
+  //         </p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
