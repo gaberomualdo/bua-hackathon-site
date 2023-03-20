1a1f53b05ab89678f3872122fa9446613c6ca04d
Rohan Biju <56479929+RoHanSolo156@users.noreply.github.com>
2023-03-20 19:56:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -144,7 +144,7 @@ export default function App() {
                     </a>
                     <a
                       href="https://forms.gle/zWjZEjJbKBD5RV78A"
-                      className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-400 bg-opacity-60 hover:bg-opacity-100 sm:px-8"
+                      className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-red-700 bg-white bg-opacity-60 hover:bg-opacity-60 sm:px-8"
                     >
                       Volunteer Registration
                     </a>
